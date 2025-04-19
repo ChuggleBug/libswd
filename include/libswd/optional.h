37b4f6ba13b2eb83eafda28edbce544119dec27b
Jacob <jacobgutierrez752@gmail.com>
2025-04-19 00:50:23 -0700

    Extend optional to support functional

--- include/libswd/optional.h
+++ include/libswd/optional.h
@@ -2,31 +2,47 @@
 #ifndef __OPTIONAL_H
 #define __OPTIONAL_H
 
-template <typename T>
-class Optional {
+#include <functional>
+
+template <typename T> class Optional {
   private:
     T value;
     bool exists;
 
-    explicit Optional() : exists(false) {}
-    explicit Optional(const T& val) : value(val), exists(true) {}
+    explicit Optional(const T &val) : value(val), exists(true) {}
 
   public:
+    explicit Optional() : exists(false) {}
 
-    static Optional<T> of(const T& value) {
-        return Optional<T>(value);
+    static Optional<T> of(const T &value) { return Optional<T>(value); }
+
+    static Optional<T> none() { return Optional<T>(); }
+
+    inline bool hasValue() const { return exists; }
+
+    inline T getValue() const { return value; }
+
+    void andThen(std::function<void(T)> func) {
+        if (hasValue()) {
+            func(getValue());
+        }
     }
 
-    static Optional<T> none() {
-        return Optional<T>();
+    void andThen(std::function<void(T)> if_func, std::function<void(void)> else_func) {
+        if (hasValue()) {
+            if_func(getValue());
+        } else {
+            else_func();
+        }
     }
 
-    bool hasValue() const {
-        return exists;
-    }
+    template <typename K> K map(std::function<K(T)> func) { return func(getValue()); }
 
-    T getValue() const {
-        return value;
+    template <typename K> K map(std::function<K(T)> func, K else_val) {
+        if (hasValue()) {
+            return func(getValue());
+        }
+        return else_val;
     }
 };
 
