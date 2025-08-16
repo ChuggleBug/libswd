33e2bcd7b47d0c8d52cb102fe5abb9ec1804bfc0
Jacob <jacobgutierrez752@gmail.com>
2025-08-15 17:32:37 -0700

    Update and document dap interface

--- c/inc/swd_dap.h
+++ c/inc/swd_dap.h
@@ -11,32 +11,57 @@
 
 typedef struct _swd_dap_t {
     swd_driver_t *driver;
-    uint8_t data_size;
-    uint8_t addr_int_bits;
     bool is_stopped;
-    bool ap_error;
-    bool is_little_endian;
+    bool _ap_error;
 } swd_dap_t;
 
 /*
- * Initialization utilities:
+ * @brief Initialize any required data required for
+ *          the dap the function
+ * @param swd_dap_t* reference of dap structure to initialize
  */
 void swd_dap_init(swd_dap_t *dap);
-void swd_dap_set_driver(swd_dap_t *dap, swd_driver_t *driver);
-
-swd_err_t swd_dap_start(swd_dap_t *dap);
-void swd_dap_stop(swd_dap_t *dap);
-void swd_dap_reset(swd_dap_t *dap);
 
 /*
- *
- *
- *
+ * @brief Assign a pin driver for the dap to use for communication to a hardware port
+ * @param swd_dap_t* reference of dap structure to assign the driver to
+ * @param swd_driver_t* reference of the driver
  */
-void swd_dap_set_jtag_to_swd(swd_dap_t *dap);
-void swd_dap_reset_target(swd_dap_t *dap);
+void swd_dap_set_driver(swd_dap_t *dap, swd_driver_t *driver);
 
+/*
+ * @brief Start the target's DAP by first initializing underlying hardware
+ *          drivers and signaling a line reset
+ * @param swd_dap_t* reference of dap structure to start
+ * @note on some targets a JTAG to SWD sequence is required. This option can be set
+ *          with SWD_CONFIG_AUTO_JTAG_SWITCH in "swd_conf.h"
+ */
+swd_err_t swd_dap_start(swd_dap_t *dap);
+
+/*
+ *  @brief Stop the target DAP and deinitialize and associated host hardware
+ *  @param swd_dap_t* reference of dap structure to stop
+ *  @note As the name implies, the dap read and writes are no longer able to be performed
+ *          The dap needs to be started again at this point
+ */
+swd_err_t swd_dap_stop(swd_dap_t *dap);
+
+/*
+ * @brief Perform a single DP/AP port read
+ * @param swd_dap_t* reference of dap structure to read form
+ * @param swd_dap_port_t DP/AP port name
+ * @param uint32_t* data buffer for read data to be written to on success
+ * @return status of dap read
+ */
 swd_err_t swd_dap_port_read(swd_dap_t *dap, swd_dap_port_t port, uint32_t *data);
+
+/*
+ * @brief Perform a single DP/AP port write
+ * @param swd_dap_t* reference of dap structure to read form
+ * @param swd_dap_port_t DP/AP port name
+ * @param uint32_t data to be written to in the port
+ * @return status of dap read
+ */
 swd_err_t swd_dap_port_write(swd_dap_t *dap, swd_dap_port_t port, uint32_t data);
 
 #endif // __SWD_DAP_H
